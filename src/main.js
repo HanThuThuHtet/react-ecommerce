e54117c2951e6ebaa3a25eb65cc75ded7c9247f3
HanThuThuHtet <htth5499@gmail.com>
2024-10-09 13:17:37 +0630

    implement homepage

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+const root = document.querySelector('#root');
+
+const title = document.createElement("p");
+title.innerText = "Product Categories";
+title.classList.add('text-2xl');
+
+root.append(title);
